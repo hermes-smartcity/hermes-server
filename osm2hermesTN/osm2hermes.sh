fd2b52125ce4c8fb7601068e3b0537f89b83024b
Miguel R. Luaces <luaces@lbd.org.es>
2015-08-31 19:21:51 +0200

    Change the folder where the software and the data is downloaded
    - Change the script to create a folder named 'target'
    - Modify all paths in the script to download and extract to the target folder
    - Create the .gitinore file to ignore the target folder

--- osm2hermesTN/osm2hermes.sh
+++ osm2hermesTN/osm2hermes.sh
@@ -3,40 +3,95 @@
 # Installation parameters
 DB_HOST=localhost
 DB_PORT=5432
-DB_USER=postgres
-PGPASSWORD=phoenix
-DB_NAME=hermes
+DB_USER=***REMOVED***
+PGPASSWORD=***REMOVED***
+DB_NAME=meco1401
+
+export PGPASSWORD
+
+# Create target
+mkdir target
 
 # Download osmosis
+echo '==='
+echo '=== Download osmosis'
+echo '==='
 wget http://bretth.dev.openstreetmap.org/osmosis-build/osmosis-latest.zip
-unzip osmosis-latest.zip
-rm -f osmosis-latest.zip
+mv osmosis-latest.zip target
+rm -rf target/osmosis
+unzip target/osmosis-latest.zip -d target/osmosis
+rm -f target/osmosis-latest.zip
 
 # Initialize our DB
+echo '==='
+echo '=== Drop DB' $DB_NAME
+echo '==='
+dropdb -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME
+
+echo '==='
+echo '=== Create DB' $DB_NAME
+echo '==='
 createdb -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME
+
+echo '==='
+echo '=== Create extensions'
+echo '==='
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c 'CREATE EXTENSION postgis; CREATE EXTENSION hstore;'
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f script/pgsnapshot_schema_0.6.sql
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f script/pgsnapshot_schema_0.6_linestring.sql
+
+echo '==='
+echo '=== pgsnapshot_schema'
+echo '==='
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f target/osmosis/script/pgsnapshot_schema_0.6.sql
+
+echo '==='
+echo '=== pgsnapshot_schema_linestring'
+echo '==='
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f target/osmosis/script/pgsnapshot_schema_0.6_linestring.sql
 
 # Download OSM data
+echo '==='
+echo '=== Download OSM data'
+echo '==='
 wget https://s3.amazonaws.com/metro-extracts.mapzen.com/acoruna_spain.osm.bz2
-bzip2 -d acoruna_spain.osm.bz2
+mv acoruna_spain.osm.bz2 target
+bzip2 -d target/acoruna_spain.osm.bz2
 rm -f acoruna_spain.osm.bz2
 
 # Import OSM data to our DB's osmosis schema
-./bin/osmosis --read-xml acoruna_spain.osm --write-pgsql host=$DB_HOST database=$DB_NAME user=$DB_USER password=$PGPASSWORD
+echo '==='
+echo '=== Import OSM Data to DB'
+echo '==='
+./target/osmosis/bin/osmosis --read-xml target/acoruna_spain.osm --write-pgsql host=$DB_HOST database=$DB_NAME user=$DB_USER password=$PGPASSWORD
+
+echo '==='
+echo '=== Create index'
+echo '==='
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c 'CREATE INDEX ways_i_tags ON ways USING gist (tags);'
 
 # Download osm2po
+echo '==='
+echo '=== Download osm2po'
+echo '==='
 wget http://osm2po.de/dld/osm2po-5.0.0.zip
-unzip osm2po-5.0.0.zip -x osm2po.config
-rm -f osm2po-5.0.0.zip
+mv osm2po-5.0.0.zip target
+rm -rf target/osm2po
+unzip target/osm2po-5.0.0.zip -x osm2po.config -d target/osm2po
+rm -f target/osm2po-5.0.0.zip
 
 # Split OSM ways into a PGRouting table
-java -Xmx1408m -jar osm2po-core-5.0.0-signed.jar prefix=es_cor tileSize=x,c cmd=c acoruna_spain.osm
-psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f es_cor/es_cor_2po_4pgr.sql
+echo '==='
+echo '===  Split OSM ways'
+echo '==='
+java -Xmx1408m -jar target/osm2po/osm2po-core-5.0.0-signed.jar workDir=target/es_cor prefix=es_cor tileSize=x,c cmd=c target/acoruna_spain.osm
+echo '==='
+echo '===  Insert into PSQL'
+echo '==='
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f target/es_cor/es_cor_2po_4pgr.sql
 
 # Execute our mapping SQL script
+echo '==='
+echo '===  OSM2HermesTN'
+echo '==='
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -f hermes.sql
 
 unset PGPASSWORD
