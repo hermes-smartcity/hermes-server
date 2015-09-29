4a58e2350eb026bd62386a3eef7813023afd54f5
cmartinez <cmartinez@enxenio.es>
2015-09-29 17:46:41 +0200

    version prueba de añadir entradas de menu recursivamente con angular. Falta solucionar muchos flecos

--- HibernateSpatialSmart/src/main/java/es/enxenio/smart/citydriver/controller/publica/PrincipalController.java
+++ HibernateSpatialSmart/src/main/java/es/enxenio/smart/citydriver/controller/publica/PrincipalController.java
@@ -1,18 +1,18 @@
-package es.enxenio.smart.citydriver.controller.publica;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import es.enxenio.smart.citydriver.controller.session.SessionManager;
-
-@Controller("principalController")
-public class PrincipalController {
-
-	@RequestMapping("/")
-	public String inicio(HttpSession session) {				
-		return "redirect:paxinaInicio/inicio";
-	}
-
-}
+//package es.enxenio.smart.citydriver.controller.publica;
+//
+//import javax.servlet.http.HttpSession;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import es.enxenio.smart.citydriver.controller.session.SessionManager;
+//
+//@Controller("principalController")
+//public class PrincipalController {
+//
+//	@RequestMapping("/")
+//	public String inicio(HttpSession session) {				
+//		return "redirect:paxinaInicio/inicio";
+//	}
+//
+//}
