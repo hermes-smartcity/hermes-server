ba4b850fb3b567d7891bad72aa1265c89d99c7b9
cmartinez <cmartinez@enxenio.es>
2015-10-29 11:02:00 +0100

    elimando del repositorio proyecto HibernateSpatialSmart

--- HibernateSpatialSmart/src/main/java/es/enxenio/smart/citydriver/model/events/eventoProcesado/dao/EventoProcesadoDao.java
+++
@@ -1,9 +0,0 @@
-package es.enxenio.smart.citydriver.model.events.eventoProcesado.dao;
-
-import es.enxenio.smart.citydriver.model.events.eventoProcesado.EventoProcesado;
-import es.enxenio.smart.citydriver.model.util.dao.GenericDao;
-
-public interface EventoProcesadoDao extends GenericDao<EventoProcesado, Long> {	
-	public EventoProcesado obterEventoProcesado();
-	public void eliminarEventosProcesados();
-}
