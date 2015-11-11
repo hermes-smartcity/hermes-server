e3cc72c06e82b1052b877002afabe1f541ae680f
Cristina <Cristina@cristina-portat>
2015-11-11 18:50:08 +0100

    Eventos pasados borrado

--- hermesManager/eventManager/src/main/java/es/udc/lbd/hermes/eventManager/EventProcessor.java
+++ hermesManager/eventManager/src/main/java/es/udc/lbd/hermes/eventManager/EventProcessor.java
@@ -36,37 +36,19 @@ import es.udc.lbd.hermes.model.util.ApplicationContextProvider;
 public class EventProcessor extends Thread {
 	
 	private static final String URI = ReadPropertiesFile.getUrlEventos();
-	private static final String URI_RECUPERAR_EVENTOS_PASADOS = ReadPropertiesFile.getUrlEventosPasados();
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
-
-	// Escucha los eventos pasados, le envian en un buffer todos los eventos durante el tiempo de desconexión
-	public void escucharEventosPasados(){
-		EventService eventService = ApplicationContextProvider.getApplicationContext().getBean("eventService", EventService.class);
-		EventoProcesado eventoProcesado = eventService.obterEventoProcesado();
-		
-		if(eventoProcesado!=null){			
-			logger.info("Escuchando eventos pasados");			
-			final Response response = establecerConexion(URI_RECUPERAR_EVENTOS_PASADOS+eventoProcesado.getEventId(),"application/json");
-			System.out.println("-url- "+URI_RECUPERAR_EVENTOS_PASADOS+eventoProcesado.getEventId());
-			procesarEventosEnviadosPasados(response);
-		}
-	}
 
 	// Escucha los eventos que se le envían en tiempo real
 	public void escucharEventos(){		
 		EventService eventService = ApplicationContextProvider.getApplicationContext().getBean("eventService", EventService.class);
 		EventoProcesado eventoProcesado = eventService.obterEventoProcesado();
-		System.out.println("-url- "+URI);
 		final Response response = establecerConexion(URI+eventoProcesado.getEventId(), "application/x-ldjson");
-		System.out.println("-url 2- "+URI);
 		logger.info("Escuchando eventos en tiempo real");	
 		procesarEventosEnviados(response);
 	}
 
 	// Comportamiento del thread
 	public void run() {	
-//		escucharEventosPasados();	
 		escucharEventos();
 		
 	}
@@ -92,76 +74,34 @@ public class EventProcessor extends Thread {
 				});
 
 		logger.info("Procesando eventos");
-
+		EventParser eventParser = new EventParser();
+		
 		while ((chunk = chunkedInput.read()) != null && !Thread.currentThread().isInterrupted()) {					
+			chunk = chunkedInput.read();
 			Object obj;
 			try {				
-				System.out.println("chunk "+chunk);
 				obj = parser.parse(chunk);
 				JSONObject eventoJSON = (JSONObject) obj;
-				InputStream is = new ByteArrayInputStream(eventoJSON.toString().getBytes());
-				EventParser eventParser = new EventParser();
+				InputStream is = new ByteArrayInputStream(eventoJSON.toString().getBytes());				
 				Event event = eventParser.parse(is);				
 				procesarEvento(event);				
 			} catch (ParseException | IOException e) {
 				logger.error("Error convirtiendo chunk a JSON: "+e);				
 				e.printStackTrace();					
-			}		
-		}
-
+			}	
+			
+		}		
 		return;
 	}
 
-	private void procesarEventosEnviadosPasados(Response response){
-
-		String chunk;
-		JSONParser parser = new JSONParser();
-
-		final ChunkedInput<String> chunkedInput = response
-				.readEntity(new GenericType<ChunkedInput<String>>() { });
-
-		chunk = chunkedInput.read();
-		while ((chunk = chunkedInput.read()) != null && !Thread.currentThread().isInterrupted()) {	
-			Object obj;
-			try {
-				
-				obj = parser.parse(chunk);
-				JSONObject eventoJSON = (JSONObject) obj;
-				InputStream is = new ByteArrayInputStream(eventoJSON.toString().getBytes());
-				EventParser eventParser = new EventParser();
-				Event event = eventParser.parse(is);	
-				procesarEvento(event);	
-//				chunk = chunkedInput.read();				
-//				obj = parser.parse(chunk);
-//				System.out.println("-- "+chunk);
-				// Me envían un array JSON con todos los eventos pasados
-//				JSONArray eventosArray = (JSONArray) obj;
-	//			for(int i=0;i<eventosArray.size();i++){
-	//				JSONObject eventoJSON = (JSONObject) eventosArray.get(i);
-	//				InputStream is = new ByteArrayInputStream(eventoJSON.toString().getBytes());
-	//				EventParser eventParser = new EventParser();
-	//				Event event = eventParser.parse(is);				
-	//				procesarEvento(event);		
-	//			}
-			} catch (ParseException | IOException e) {
-				logger.error("Error convirtiendo chunk a JSON: "+e);				
-				e.printStackTrace();					
-			}
-		}
-		chunkedInput.close();
+	// Almacenamos los diferentes tipos de eventos en la BD
+	private void procesarEvento(Event event){					
+		EventType tipoEvento = EventType.getTipo((String) event.getEventType());
 		
-				return;
-		}
+		EventStrategy estrategia = EventFactory.getEvent(tipoEvento);
+		estrategia.processEvent(event);
+		logger.info("Guardado el evento con Event-Type: "+tipoEvento.getName());
+		
 
-		// Almacenamos los diferentes tipos de eventos en la BD
-		private void procesarEvento(Event event){			
-			
-			EventType tipoEvento = EventType.getTipo((String) event.getEventType());
-			
-			EventStrategy estrategia = EventFactory.getEvent(tipoEvento);
-			estrategia.processEvent(event);
-			logger.info("Guardado el evento con Event-Type: "+tipoEvento.getName());
-			
-
-		}
+	}
 	}
