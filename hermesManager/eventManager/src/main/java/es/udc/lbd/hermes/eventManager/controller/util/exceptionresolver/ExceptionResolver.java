828395981ca09748ce0e6523dbb7114e0c919778
cmartinez <cristinacmp1988@gmail.com>
2015-12-22 20:01:10 +0100

    Movido tiles y jspx para event manager Configurado para que lance jetty. Falte que muestre las jspx pero como no lo tenía configurado para tal (lo cambié hoy o ayer) lo dejo así de momento hasta mañana ya que sólo vamos a hacer peticiones a los controladores (REST)

--- hermesManager/eventManager/src/main/java/es/udc/lbd/hermes/eventManager/controller/util/exceptionresolver/ExceptionResolver.java
+++ hermesManager/eventManager/src/main/java/es/udc/lbd/hermes/eventManager/controller/util/exceptionresolver/ExceptionResolver.java
@@ -4,16 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-import es.udc.lbd.hermes.model.util.jackson.CustomGeometryDeserializer;
-
 public class ExceptionResolver implements HandlerExceptionResolver {
 
-//	private static final Logger log = LoggerFactory.getLogger(ExceptionResolver.class);
 	static Logger log = Logger.getLogger(ExceptionResolver.class);
 	
 	@Override
