bc5659ae3a1617f79af6185080db3dafb8366bf0
cmartinez <cristinacmp1988@gmail.com>
2016-01-27 16:39:50 +0100

    Anadida clave y usuario de recordatorio al script

--- hermesManager/database/src/sql/20160127-CreateUserData.sql
+++ hermesManager/database/src/sql/20160127-CreateUserData.sql
@@ -1,5 +1,5 @@
 -- script update - postgres --
 
--- usuario_web --
+-- usuario_web (usuario:user - password:user) --
 INSERT INTO usuario_web (rol, email, password) VALUES
     ('ADMIN', 'user3', '47a733d60998c719cf3526ae7d106d13');
