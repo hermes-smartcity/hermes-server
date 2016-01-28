1c55527dbd71006392925ed2fb08277adead708c
cmartinez <cristinacmp1988@gmail.com>
2016-01-28 18:18:13 +0100

    Encriptación con BCryptPasswordEncoder para loguearse. Falta la parte de tokens

--- hermesManager/database/src/sql/20160127-CreateUserData.sql
+++ hermesManager/database/src/sql/20160127-CreateUserData.sql
@@ -1,5 +1,9 @@
 -- script update - postgres --
 
--- usuario_web (usuario:user - password:user) --
+-- usuario_web (usuario:user - password:user) Para MD5 --
 INSERT INTO usuario_web (rol, email, password) VALUES
     ('ADMIN', 'user3', '47a733d60998c719cf3526ae7d106d13');
+    
+ -- usuario_web (usuario:user - password:user) Para BCryptPasswordEncoder --
+INSERT INTO usuario_web (rol, email, password) VALUES
+    ('ADMIN', 'user3', '$2a$10$WPe2V9MC1loxbiLIsqQE9e4hYnUq37dHpPzKAcv1RVUnfwHiSfJyG');
