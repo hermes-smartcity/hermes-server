100c649e59acb6d452f558327e1373e7810cf9d3
Miguel R. Luaces <luaces@lbd.org.es>
2016-02-15 12:49:31 +0100

    Changed default admin user

--- hermesManager/database/src/sql/20160127-CreateUserData.sql
+++ hermesManager/database/src/sql/20160127-CreateUserData.sql
@@ -1,12 +1,4 @@
 -- script update - postgres --
 
--- usuario_web (usuario:user - password:user) Para MD5 --
-INSERT INTO usuario_web (rol, email, password) VALUES
-    ('ADMIN', 'user', '47a733d60998c719cf3526ae7d106d13');
-    
- -- usuario_web (usuario:user - password:user) Para BCryptPasswordEncoder --
-INSERT INTO usuario_web (rol, email, password) VALUES
-    ('ADMIN', 'user', '$2a$10$WPe2V9MC1loxbiLIsqQE9e4hYnUq37dHpPzKAcv1RVUnfwHiSfJyG');
-    
--- Con spring security 4
-UPDATE usuario_web SET rol = 'ROLE_ADMIN';
+ -- usuario_web (usuario: admin - password: $$admin$$) Para BCryptPasswordEncoder --
+INSERT INTO usuario_web (rol, email, password) VALUES ('ROLE_ADMIN', 'admin', '$2a$10$5dfh2cas5VPqJzlbUMezH.x3UvkEvYGyuYuK18vLSBwWGDhwLUN.S');
