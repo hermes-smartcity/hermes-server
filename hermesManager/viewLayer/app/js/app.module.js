5c47b5be2e27d07c0122a2e30b720cfc4a9e760a
Leticia Riestra Ainsua <lriestra@lbd.org.es>
2016-04-01 12:42:44 +0200

    Fila de indicadores en System Logs / poner la tabla en un panel

--- hermesManager/viewLayer/app/js/app.module.js
+++ hermesManager/viewLayer/app/js/app.module.js
@@ -250,6 +250,56 @@
 			templateUrl:'partials/systemLogs/systemLogs.html',
 			controller: 'SystemLogsController',
 			controllerAs: 'vm',
+			resolve: {
+				eventsType: ['eventsService', function(eventsService) {
+					return eventsService.getEvensType();
+				}],
+				usuarios: ['eventsService', function(eventsService) {
+					return eventsService.getUsuarios();
+				}],
+				measurementsType: ['eventsService', function(eventsService) {
+					return eventsService.getMeasurementsType();
+				}],
+				totalMUsers: ['userService', function(userService) {
+					return userService.getTotalMUsers();
+				}],
+				totalWebUsers: ['userService', function(userService) {
+					return userService.getTotalWebUsers();
+				}],
+				numberActiveUsers: ['userService', function(userService) {
+					return userService.getNumberActiveUsers();
+				}],
+				eventoProcesado: ['eventsService', function(eventsService) {
+					return eventsService.getEventoProcesado();
+				}],
+				eventsToday: ['eventsService', function(eventsService) {
+					return eventsService.getEventsToday();
+				}],
+				totalL: ['eventsService', function(eventsService) {
+					return eventsService.getTotalVLocations();
+				}],
+				totalDS: ['eventsService', function(eventsService) {
+					return eventsService.getTotalDataScts();
+				}],
+				totalM: ['eventsService', function(eventsService) {
+					return eventsService.getTotalMeasurements();
+				}],
+				totalDF: ['eventsService', function(eventsService) {
+					return eventsService.getTotalDriversF();
+				}],
+				totalSTD: ['eventsService', function(eventsService) {
+					return eventsService.getTotalStepsData();
+				}],
+				totalSLD: ['eventsService', function(eventsService) {
+					return eventsService.getTotalSleepData();
+				}],
+				totalHRD: ['eventsService', function(eventsService) {
+					return eventsService.getTotalHeartRateData();
+				}],
+				totalCD: ['eventsService', function(eventsService) {
+					return eventsService.getTotalContextData();
+				}]
+			},
 			data: {
 			      permissions: {
 			          only: ['ROLE_ADMIN'],
