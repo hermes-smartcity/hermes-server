3571cf3beece263bc3a12b2cc2338397655d7327
Leticia Riestra Ainsua <lriestra@lbd.org.es>
2016-04-29 09:56:19 +0200

    Quitar GET_INFORMATION_LINK de SmartCitizien

--- hermesManager/model/src/main/java/es/udc/lbd/hermes/model/dataservice/Method.java
+++ hermesManager/model/src/main/java/es/udc/lbd/hermes/model/dataservice/Method.java
@@ -7,7 +7,7 @@ public enum Method {
 	GET_INFORMATION_LINK, AGGREGATE_MEASUREMENT, COMPUTE_ROUTE;
 	
 	public static EnumSet<Method> smartDriver = EnumSet.of(GET_INFORMATION_LINK, AGGREGATE_MEASUREMENT);
-	public static EnumSet<Method> smartCitizen = EnumSet.of(GET_INFORMATION_LINK, COMPUTE_ROUTE);
+	public static EnumSet<Method> smartCitizen = EnumSet.of(COMPUTE_ROUTE);
 	
 	public String getName(){		
 		return this.name();
