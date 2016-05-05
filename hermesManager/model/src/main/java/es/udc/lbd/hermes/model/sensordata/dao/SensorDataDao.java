1ab5956f4ba779625cdc7f5cbcea8b05a0393c77
Miguel R. Luaces <luaces@lbd.org.es>
2016-05-05 11:43:15 +0200

    Reduce number of items when fetching SensorData

--- hermesManager/model/src/main/java/es/udc/lbd/hermes/model/sensordata/dao/SensorDataDao.java
+++ hermesManager/model/src/main/java/es/udc/lbd/hermes/model/sensordata/dao/SensorDataDao.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.List;
 
 import es.udc.lbd.hermes.model.sensordata.SensorData;
+import es.udc.lbd.hermes.model.sensordata.SensorDataDTO;
 import es.udc.lbd.hermes.model.sensordata.SensorDataType;
 import es.udc.lbd.hermes.model.util.dao.GenericDao;
 
@@ -11,7 +12,7 @@ public interface SensorDataDao extends GenericDao<SensorData, Long>{
 
 	public SensorData findLast(Long userId, String type);
 	
-	public List<SensorData> informacionPorDia(SensorDataType tipo,Long idUsuario, Calendar fechaIni, Calendar fechaFin);
+	public List<SensorDataDTO> informacionPorDia(SensorDataType tipo,Long idUsuario, Calendar fechaIni, Calendar fechaFin);
 	
-	public Long getCadaCuantos(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin);
+	public Long countRows(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin);
 }
--- hermesManager/model/src/main/java/es/udc/lbd/hermes/model/sensordata/dao/SensorDataDaoImpl.java
+++ hermesManager/model/src/main/java/es/udc/lbd/hermes/model/sensordata/dao/SensorDataDaoImpl.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.SQLQuery;
+import org.hibernate.transform.Transformers;
+import org.hibernate.type.BigIntegerType;
+import org.hibernate.type.CalendarType;
+import org.hibernate.type.CustomType;
+import org.hibernate.type.StringType;
 import org.springframework.stereotype.Repository;
 
-import es.udc.lbd.hermes.model.sensordata.Row;
 import es.udc.lbd.hermes.model.sensordata.SensorData;
+import es.udc.lbd.hermes.model.sensordata.SensorDataDTO;
 import es.udc.lbd.hermes.model.sensordata.SensorDataType;
 import es.udc.lbd.hermes.model.util.dao.GenericDaoHibernate;
+import es.udc.lbd.hermes.model.util.hibernate.NumericArrayUserType;
 
 @Repository
 public class SensorDataDaoImpl extends GenericDaoHibernate<SensorData, Long> implements SensorDataDao{
@@ -32,11 +38,11 @@ public class SensorDataDaoImpl extends GenericDaoHibernate<SensorData, Long> imp
 	}
 	
 	@SuppressWarnings("unchecked")
-	public Long getCadaCuantos(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin){
+	public Long countRows(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin){
 		
 		String queryStr =  "select count(*)  from SensorData d " +
 						"where d.startime > :fechaIni and d.endtime < :fechaFin " +
-						"and d.typesensor LIKE :tipo order by d.startime";
+						"and d.typesensor LIKE :tipo";
 
 		if(idUsuario!=null)
 			queryStr += " and d.usuarioMovil.id = :idUsuario ";
@@ -53,18 +59,50 @@ public class SensorDataDaoImpl extends GenericDaoHibernate<SensorData, Long> imp
 		return (Long) query.uniqueResult();
 	}
 	
+//	@SuppressWarnings("unchecked")
+//	public List<SensorData> informacionPorDia(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin){
+//		
+//		String queryStr =  "from SensorData d " +
+//						"where d.startime > :fechaIni and d.endtime < :fechaFin " +
+//						"and d.typesensor LIKE :tipo order by d.startime";
+//
+//		if(idUsuario!=null)
+//			queryStr += " and d.usuarioMovil.id = :idUsuario ";
+//		
+//		Query query = getSession().createQuery(queryStr);
+//				
+//		query.setCalendar("fechaIni", fechaIni);
+//		query.setCalendar("fechaFin", fechaFin);
+//		query.setParameter("tipo", tipo.getName());
+//		
+//		if (idUsuario!=null)
+//			query.setParameter("idUsuario", idUsuario);
+//		
+//		return (List<SensorData>) query.list();
+//	}
+	
 	@SuppressWarnings("unchecked")
-	public List<SensorData> informacionPorDia(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin){
+	public List<SensorDataDTO> informacionPorDia(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin){
+		String queryStr =  "select id, typesensor, startime, endtime, values, idusuariomovil from sensordata d " +
+						"where cast(extract (epoch from d.startime) as integer) % :cadacuantos = 0 "
+						+ "and d.startime > :fechaIni and d.endtime < :fechaFin "
+						+ "and d.typesensor LIKE :tipo ";
+		if(idUsuario!=null) {
+			queryStr += "and d.usuarioMovil.id = :idUsuario ";
+		}
 		
-		String queryStr =  "from SensorData d " +
-						"where d.startime > :fechaIni and d.endtime < :fechaFin " +
-						"and d.typesensor LIKE :tipo order by d.startime";
-
-		if(idUsuario!=null)
-			queryStr += " and d.usuarioMovil.id = :idUsuario ";
-		
-		Query query = getSession().createQuery(queryStr);
-				
+		queryStr += "order by d.startime";
+		System.out.println("-  --- "+queryStr);
+		SQLQuery query = getSession().createSQLQuery(queryStr);
+		Long numeroDeFilas = countRows(tipo, idUsuario, fechaIni, fechaFin);
+		Long cadaCuantos = numeroDeFilas / 1000;
+		query.setParameter("cadacuantos", cadaCuantos);
+		query.addScalar("id", BigIntegerType.INSTANCE);
+		query.addScalar("typesensor", StringType.INSTANCE);		
+		query.addScalar("startime", CalendarType.INSTANCE);
+		query.addScalar("endtime", CalendarType.INSTANCE);
+		query.addScalar("values", new CustomType(new NumericArrayUserType()));		
+		query.addScalar("idusuariomovil", BigIntegerType.INSTANCE);
 		query.setCalendar("fechaIni", fechaIni);
 		query.setCalendar("fechaFin", fechaFin);
 		query.setParameter("tipo", tipo.getName());
@@ -72,29 +110,7 @@ public class SensorDataDaoImpl extends GenericDaoHibernate<SensorData, Long> imp
 		if (idUsuario!=null)
 			query.setParameter("idUsuario", idUsuario);
 		
-		return (List<SensorData>) query.list();
+		query.setResultTransformer(Transformers.aliasToBean(SensorDataDTO.class));
+		return (List<SensorDataDTO>) query.list();
 	}
-	
-//	@SuppressWarnings("unchecked")
-//	public List<SensorData> informacionPorDia(SensorDataType tipo, Long idUsuario, Calendar fechaIni, Calendar fechaFin){
-//		// Como no funcionaba borre lo de arriba para simplificar. Incluir! 
-//		String queryStr =  "from SensorData d " +
-//						"where cast(extract (epoch from d.startime) as integer) % 37 = 0"
-//						+ " order by d.startime";
-//
-//		System.out.println("-  --- "+queryStr);
-//		if(idUsuario!=null)
-//			queryStr += " and d.usuarioMovil.id = :idUsuario ";
-//		
-////		Query query = getSession().createQuery(queryStr);
-//		SQLQuery query = getSession().createSQLQuery(queryStr);		
-////		query.setCalendar("fechaIni", fechaIni);
-////		query.setCalendar("fechaFin", fechaFin);
-////		query.setParameter("tipo", tipo.getName());
-//		
-//		if (idUsuario!=null)
-//			query.setParameter("idUsuario", idUsuario);
-//		
-//		return (List<SensorData>) query.list();
-//	}
 }
