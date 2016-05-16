0063e930c78e9bd86cdeb8311fd4e56ba35237df
Miguel R. Luaces <luaces@lbd.org.es>
2016-05-16 23:12:21 +0200

    Missing table in creation script

--- hermesManager/database/src/sql/04-PSQL-CreateEventsSchema.sql
+++ hermesManager/database/src/sql/04-PSQL-CreateEventsSchema.sql
@@ -314,3 +314,19 @@ CREATE TABLE userlocations (
   CONSTRAINT iduserlocations_fk_usuario FOREIGN KEY (idUsuarioMovil) REFERENCES usuario_movil(id) ON DELETE CASCADE
 )
 ;
+
+drop table if exists setting cascade;
+drop sequence if exists setting_id_seq cascade;
+create sequence setting_id_seq;
+
+CREATE TABLE setting (
+  id bigint NOT NULL DEFAULT nextval('setting_id_seq'::regclass),
+  name VARCHAR(50) NOT NULL,
+  valueChar VARCHAR(100),
+  valueNumber decimal,
+  type VARCHAR(20) NOT NULL,
+  CONSTRAINT idsetting_pk PRIMARY KEY (id)
+)
+;
+		
+INSERT INTO setting(name,valueNumber,type) VALUES ('limitQuery',1000, 'number');
