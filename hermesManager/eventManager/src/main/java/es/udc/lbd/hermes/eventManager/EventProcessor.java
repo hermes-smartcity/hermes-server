6ab781d04e4f525876823932c0e71dd7ea1b981b
Miguel R. Luaces <luaces@lbd.org.es>
2016-05-16 23:36:22 +0200

    Improve error messages in Ztreamy Event Parser

--- hermesManager/eventManager/src/main/java/es/udc/lbd/hermes/eventManager/EventProcessor.java
+++ hermesManager/eventManager/src/main/java/es/udc/lbd/hermes/eventManager/EventProcessor.java
@@ -90,21 +90,25 @@ public class EventProcessor extends Thread {
 		try {
 			Event event = eventParser.parse(chunk);
 			if (!"ztreamy-command".equals(event.getSyntax())) {
-				if (event.getEventType() != null) {
-					EventType tipoEvento = EventType.getTipo((String) event.getEventType());
-					EventStrategy estrategia = EventFactory.getStrategy(tipoEvento);
-					if (estrategia != null) {
-						try {
-							estrategia.processEvent(event);
-						} catch (ClassCastException e) {
-							logger.error("Event-Type no coincide con el tipo especificado en el body\n"+chunk, e);
+				if (event.getEventData() != null) { 
+					if (event.getEventType() != null) {
+						EventType tipoEvento = EventType.getTipo((String) event.getEventType());
+						EventStrategy estrategia = EventFactory.getStrategy(tipoEvento);
+						if (estrategia != null) {
+							try {
+								estrategia.processEvent(event);
+							} catch (ClassCastException e) {
+								logger.error("Event-Type no coincide con el tipo especificado en el body\n"+chunk, e);
+							}
+							logger.info("Guardado el evento con Event-Type: " + tipoEvento.getName());
+						} else {
+							logger.warn("EventType desconocido: " + chunk);
 						}
-						logger.info("Guardado el evento con Event-Type: " + tipoEvento.getName());
 					} else {
-						logger.warn("EventType desconocido: " + chunk);
+						logger.warn("EventType is null: " + chunk);
 					}
 				} else {
-					logger.warn("EventType is null: " + chunk);
+					logger.error("Error convirtiendo chunk a JSON: "+chunk);
 				}
 			} else {
 				logger.info("Evento ignorado: " + chunk);
